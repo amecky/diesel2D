268a7d0d3b568e2e186e656bf46c22150bd96ee5
meckya <amecky@gmail.com>
2014-04-18 14:45:38 +0200

    removing dead code

--- base/GameObject.h
+++ base/GameObject.h
@@ -5,31 +5,6 @@
 
 namespace ds {
 
-class TFunctor {
-
-public:
-	virtual void updateState(Sprite* sprite,float elapsed) {}
-};	
-
-template <class TClass> 
-class TSpecificFunctor : public TFunctor
-   {
-   private:
-      void (TClass::*fpt)(Sprite*,float);   // pointer to member function
-      TClass* pt2Object;                  // pointer to object
-
-   public:
-
-	      // constructor - takes pointer to an object and pointer to a member and stores
-      // them in two private variables
-      TSpecificFunctor(TClass* _pt2Object, void(TClass::*_fpt)(Sprite*,float))
-         { pt2Object = _pt2Object;  fpt=_fpt; };
-
-      // override function "Call"
-      virtual void updateState(Sprite* sprite,float elapsed)
-        { (*pt2Object.*fpt)(sprite,elapsed);};             // execute member function
-   };
-
 class ParticleManager;
 class AssetCompiler;
 
@@ -38,10 +13,8 @@ class AssetCompiler;
 // -------------------------------------------------------
 class GameObject {
 
-typedef std::vector<TFunctor*> Callbacks;
-
 public:
-	GameObject() , m_ActiveCallback(-1) {}
+	GameObject() {}
 	virtual ~GameObject() {}	
 	void setRenderer(Renderer* renderer) {
 		m_Renderer = renderer;
@@ -73,16 +46,7 @@ public:
 	const bool hasEvents() const {
 		return m_Events.num() > 0;
 	}
-	void registerFN(TFunctor* fn) {
-		m_Callbacks.push_back(fn);
-	}
-	//void call(int i){
-		//CALL_MEMBER_FN(*this, m_Callback) (0, 3.14f);
-	//}
 protected:
-	Callbacks m_Callbacks;
-	int m_ActiveCallback;
-	//GameObjectFn m_Callback;
 	EventStream m_Events;
 	Renderer* m_Renderer;
 	SpriteCollisionManager* m_CollisionManager;
